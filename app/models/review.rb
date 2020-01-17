d5b04d5299943410c91c50dc9d4ee8bce11be21e
Judah Barton <judah.barton@yahoo.com>
2020-01-17 12:19:45 -0800

    add callbacks and validations to my project

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,13 @@
 class Review < ApplicationRecord
   belongs_to :product
+
+  validates :author, presence: true
+    validates_length_of :author, maximum: 100
+
+    before_save(:titleize_review)
+
+    private
+        def titleize_review
+          self.author = self.author.titleize
+        end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 describe Product do
   it { should have_many(:reviews) }
+  it { should validate_presence_of :name }
+  it { should validate_length_of(:name).is_at_most(100) }
+  it("titleizes the name of an product") do
+    product = Product.create({name: "milk", cost: 4, country_of_origin: "United States"})
+    expect(product.name()).to(eq("Milk"))
+  end
 end
